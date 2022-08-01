2c1d04c818077bc770b847ec5c520a5b096e36cc
K0lumna <michaljaniec12@wp.pl>
2022-08-01 22:23:07 +0200

    list testing fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/header/Header";
 import Monitor from "./components/monitor/Monitor";
 
 const App = () => {
-  const [odpowiedzi, setOdpowiedzi] = useState([['Szymczak is the best', 69], ['bajo jajo', 0]])
+  const [odpowiedzi] = useState([['Szymczak is the best', 69], ['bajo jajo', 0]])
   return (
     <div className=" container flex flex-col justify-between items-center min-h-screen w-full">
       <Header />
