211d3dd66b16b1cf2abe03cc722e39d5245569e3
K0lumna <michaljaniec12@wp.pl>
2022-08-03 22:01:50 +0200

    new system test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
-import Form from "./components/form/Form";
-import Button from "./components/button/Button";
-import Header from "./components/header/Header";
-import Monitor from "./components/monitor/Monitor";
+import React, { useState, useRef } from "react";
+import Monitor from "./components/monitor/Monitor"
 
 const App = () => {
-  const [odpowiedzi] = useState([['Szymczak is the best', 69], ['bajo jajo', 0]])
+  const [odpowiedzi, setOdpowiedzi] = useState([]);
+  const [wyniki, setWyniki] = useState(['lol']);
+
+  const odpNameRef = useRef();
+  const wynNameRef = useRef();
+
+  const addOdp = (e) => {
+    const name = odpNameRef.current.value;
+    if (name) {
+      setOdpowiedzi(i => {
+        return name;
+      })
+    }
+    odpNameRef.current.value = null;
+  }
+
+  const valOdp = (e) => {
+    const name = odpowiedzi;
+    const val =  wynNameRef.current.value;
+
+    if (name === val) {
+      setWyniki(i => {
+        return name;
+      })
+    } else {
+      setWyniki(i => {
+        return 'noob';
+      })
+    }
+  }
+
   return (
-    <div className=" container flex flex-col justify-between items-center min-h-screen w-full">
-      <Header />
-      <Form />
-      <Monitor odpowiedzi={odpowiedzi} />
-      <Button />
+    <div className=" container flex flex-col justify-evenly items-center min-h-screen w-full">
+      <div className=" flex w-full justify-center items-center">
+        <input ref={odpNameRef} className=" w-full bg-gray-200 h-14 rounded-2xl pl-4" placeholder="Ustaw odpowiedź" type='text'/>
+        <button onClick={addOdp} className=" bg-green-500 rounded-2xl w-20 h-14 ml-4">Wyślij</button>
+      </div>
+      <Monitor wyniki={wyniki} />
+      <div className=" flex w-full justify-center items-center">
+        <input ref={wynNameRef} className=" w-full bg-gray-200 h-14 rounded-2xl pl-4" placeholder="Wpisz odpowiedź" type='text'/>
+        <button onClick={valOdp} className=" bg-green-500 rounded-2xl w-20 h-14 ml-4">Wyślij</button>
+      </div>
     </div>
   );
 }
--- src/components/monitor/Monitor.js
+++ src/components/monitor/Monitor.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
-const Monitor = ({odpowiedzi}) => {
-  return (
-    <div className='flex justify-center items-center'>
-        <div className=' flex justify-evenly items-center bg-gray-200 shadow-xl w-[1000px] h-[500px] rounded-3xl'>
-          <div className=' flex justify-evenly items-center flex-col h-[200px]'>
-            <h1>{odpowiedzi[0][0]}</h1>
-            <h1>{odpowiedzi[1][0]}</h1>
-          </div>
-          <div className=' flex justify-evenly items-center flex-col h-[200px]'>
-            <h1>{odpowiedzi[0][1]}</h1>
-            <h1>{odpowiedzi[1][1]}</h1>
-          </div>
-        </div>
-    </div>
-  )
+const Monitor = ({wyniki}) => {
+  if (wyniki === 'noob') {
+    return (
+      <div className=' flex justify-evenly items-center bg-gray-200 w-full h-[500px] rounded-3xl'>
+        <h1 className=' text-[6rem] text-red-700'>{wyniki}</h1>
+      </div>
+    )
+  } else {
+    return (
+      <div className=' flex justify-evenly items-center bg-gray-200 w-full h-[500px] rounded-3xl'>
+        <h1 className=' text-3xl'>{wyniki}</h1>
+      </div>
+    )
+  }
 }
 
 export default Monitor
